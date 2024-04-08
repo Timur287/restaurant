2a5b5a64f44e27f2874a6d2bf3ecda0804d6396d
Timur287 <72490106+Timur287@users.noreply.github.com>
2024-04-09 01:10:29 +0300

    Update index.php
    Исправил расположение значений характеристик

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require 'connect.php';
+require 'functions.php';
 session_start();
 $admin_id = $_SESSION['admin_id'];
 if (!isset($admin_id)) {
@@ -20,11 +22,109 @@ if (!isset($admin_id)) {
 <?php include 'header.php'; ?>
 <main id="main">
     <div class="title">Панель администратора</div>
+    <div class="cards">
+        <?php
+        $conn = openConn();
+        ?>
+        <div class="card">
+            <?php
+            $result = $conn->query("SELECT COUNT(*) as total FROM `reservations`");
+            if ($result->num_rows != 0) {
+                ?>
+                <div class="description">
+                    <label class="label-desc">За всё время забронировали столики:</label>
+                    <div><?= $result->fetch_assoc()['total'] ?></div>
+                </div>
+                <?php
+            }
+            else {
+                ?>
+                <div class="description">Столики ещё не бронировали</div>
+                <?php
+            }
+            ?>
+        </div>
+        <div class="card">
+            <?php
+            $result = $conn->query("SELECT COUNT(*) as total FROM `orders`");
+            if ($result->num_rows != 0) {
+                ?>
+                <div class="description">
+                    <label class="label-desc">За всё время сделали заказ:</label>
+                    <div><?= $result->fetch_assoc()['total'] ?></div>
+                </div>
+                <?php
+            }
+            else {
+                ?>
+                <div class="description">Заказы ещё не делали</div>
+                <?php
+            }
+            ?>
+        </div>
+        <div class="card">
+            <?php
+            $result = $conn->query("SELECT COUNT(*) as total FROM `tables`");
+            if ($result->num_rows != 0) {
+                ?>
+                <div class="description">
+                    <label class="label-desc">Всего столиков:</label>
+                    <div><?= $result->fetch_assoc()['total'] ?></div>
+                </div>
+                <?php
+            }
+            else {
+                ?>
+                <div class="description">Столиков ещё нет</div>
+                <?php
+            }
+            ?>
+        </div>
+        <div class="card">
+            <?php
+            $result = $conn->query("SELECT COUNT(*) as total FROM `dishes`");
+            if ($result->num_rows != 0) {
+                ?>
+                <div class="description">
+                    <label class="label-desc">Всего блюд в меню:</label>
+                    <div><?= $result->fetch_assoc()['total'] ?></div>
+                </div>
+                <?php
+            }
+            else {
+                ?>
+                <div class="description">Блюд ещё нет</div>
+                <?php
+            }
+            ?>
+        </div>
+        <div class="card">
+            <?php
+            $result = $conn->query("SELECT SUM(dish_price) as total_sum FROM `order_items`");
+            if ($result->num_rows != 0) {
+                ?>
+                <div class="description">
+                    <label class="label-desc">За всё время продали блюд на:</label>
+                    <div>$<?= $result->fetch_assoc()['total_sum'] ?></div>
+                </div>
+                <?php
+            }
+            else {
+                ?>
+                <div class="description">Ещё ничего не продали</div>
+                <?php
+            }
+            closeConn($conn);
+            ?>
+        </div>
+    </div>
 
 </main>
 <script src="js/script.js"></script>
-<script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
-<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
+<script type="module"
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule
+        src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
 
